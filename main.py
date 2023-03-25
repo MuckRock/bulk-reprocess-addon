2253135b565b73c9de77a8602c6708a364dfd8d0
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-03-24 19:17:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,19 @@
 """
-This is a hello world add-on for DocumentCloud.
-
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
+This DocumentCloud Add-On allows you to bulk reprocress documents on DocumentCloud
 """
 
-from documentcloud.addon import AddOn
+from documentcloud.addon import SoftTimeOutAddOn
 
-
-class HelloWorld(AddOn):
-    """An example Add-On for DocumentCloud."""
+class Reprocess(SoftTimeOutAddOn):
+    """Force reprocress documents given ocr, ocr engine, and language"""
 
     def main(self):
         """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
-
-        self.set_message("Hello World start!")
-
-        # add a hello note to the first page of each selected document
+        """ ocr = self.data.get("ocr")
+        ocr_engine = self.data.get("ocr_engine")
+        document_language = self.data.get("document_language")"""
         for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
-
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
+            document.procress()
 
 
 if __name__ == "__main__":
