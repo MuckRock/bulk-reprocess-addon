0ef93f3d342fc4bfea65b6e44095e5e149e42506
s <102841251+duckduckgrayduck@users.noreply.github.com>
2023-04-24 16:16:05 -0500

    Ran black & pylint

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 This DocumentCloud Add-On allows you to bulk reprocress documents on DocumentCloud
 """
 
-import requests
 from documentcloud.addon import SoftTimeOutAddOn
 from documentcloud.constants import BULK_LIMIT
-from documentcloud.toolbox import grouper, requests_retry_session
+from documentcloud.toolbox import grouper
 
 class Reprocess(SoftTimeOutAddOn):
     """Force reprocress documents given ocr, ocr engine, and language"""
@@ -19,14 +18,10 @@ class Reprocess(SoftTimeOutAddOn):
 
         for doc_group in grouper(self.get_documents(), BULK_LIMIT):
             doc_group = [
-                {   
-                    "id": d.id, 
-                    "force_ocur": ocr, 
-                    "language": lang
-                }
-                for d in doc_group
+                {"id": d.id, "force_ocur": ocr, "language": lang} for d in doc_group
             ]
-            resp = self.client.post("documents/process/", json=doc_group)
+            self.client.post("documents/process/", json=doc_group)
+
 
 if __name__ == "__main__":
     Reprocess().main()
