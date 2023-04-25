630dcdc4cd2c487f3117c97ebce7edfb9db4f96d
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-04-25 14:00:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,16 +11,14 @@ class Reprocess(SoftTimeOutAddOn):
 
     def main(self):
         """The main add-on functionality goes here."""
-        ocr = self.data.get("force_ocr")
-        if ocr is None:
-            ocr = False
+        ocr = self.data.get("force_ocr", False)
         lang = self.data["language"]
-
-        for doc_group in grouper(self.get_documents(), BULK_LIMIT):
-            doc_group = [
-                {"id": d.id, "force_ocur": ocr, "language": lang} for d in doc_group
-            ]
-            self.client.post("documents/process/", json=doc_group)
+        if self.data["sure"]:
+            for doc_group in grouper(self.get_documents(), BULK_LIMIT):
+                doc_group = [
+                    {"id": d.id, "force_ocur": ocr, "language": lang} for d in doc_group
+                ]
+                self.client.post("documents/process/", json=doc_group)
 
 
 if __name__ == "__main__":
