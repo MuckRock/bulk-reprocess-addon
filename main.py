cb17eb16a8ac3e3f63cad14a49e112c8e6d19d87
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2026-09-04 14:53:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class Reprocess(SoftTimeOutAddOn):
                     if d is not None
                 ]
                 self.client.post("documents/process/", json=doc_group)
-                time.sleep(1)
+                time.sleep(5)
         else:
             self.set_message(
                 "You did not select sure, this Add-On did not do anything."
